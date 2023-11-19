ebf53b2f6c8985f3680afe1fbf8bc89cee4e404b
Ha Phan <ha.markus.phan@gmail.com>
2023-11-19 14:26:18 +0000

    add geolocatin API

--- assets/script.js
+++ assets/script.js
@@ -3,26 +3,39 @@ var searchInputEl = document.getElementById("search-input");
 // console.log(searchBtnEl);
 // console.log(searchInputEl);
 // var searchInputText = searchInputEl.value.trim();
+
+var key = "87251923eddd66bd3c797f59bf9006ff";
+
 searchBtnEl.addEventListener('click',function(e){
     e.preventDefault();
     var searchInputText = searchInputEl.value.trim();
     console.log(searchInputEl);
     console.log(searchInputText);
+    var geoApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInputText + "&limit=5&appid=" + key;
+    console.log(geoApiURL);
     
+    // var latitude = 51.5072;
+    // var longitude = 0.1276;
+
+    fetch(geoApiURL)
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+        console.log(latitude);
+        console.log(longitude);
+    });
 });
 
 
-var key = "87251923eddd66bd3c797f59bf9006ff";
-var latitude = 51.5072;
-var longitude = 0.1276;
 
-var geoApiURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+// var weatherApiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude+"&appid=" + key;
 
-var weatherApiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude+"&appid=" + key;
-
-fetch(apiURL)
-.then(function(response){
-    return response.json();
-}).then(function(data){
-    // console.log(data);
-});
+// fetch(weatherApiURL)
+// .then(function(response){
+//     return response.json();
+// }).then(function(data){
+//     // console.log(data);
+// });
