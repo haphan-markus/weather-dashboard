9f5c053e895e314de419f87847885df9cb3030f0
Ha Phan <ha.markus.phan@gmail.com>
2023-11-19 23:31:27 +0000

    create click event to show weather information for each button saved in search history

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,7 @@ var searchBtnEl = document.getElementById('search-button');
 var searchInputEl = document.getElementById("search-input");
 var key = "87251923eddd66bd3c797f59bf9006ff"; //API key
 var todayweatherEl = document.getElementById("today");
-// console.log(todayweatherEl);
 var forecastweatherEl = document.getElementById("forecast");
-// console.log(forecastweatherEl.getElementsByClassName("card-title"));
 var historyEl = document.getElementById("history");
 
 if (JSON.parse(window.localStorage.getItem(("searchHistory"))) == null){
@@ -18,6 +16,7 @@ if (JSON.parse(window.localStorage.getItem(("searchHistory"))) == null){
         historyBtnEl.setAttribute('class','btn btn-secondary search-button form-control');
         historyEl.appendChild(historyBtnEl);
     }
+    console.log(historyEl);
 }
 
 searchBtnEl.addEventListener('click',function(e){
@@ -70,3 +69,48 @@ searchBtnEl.addEventListener('click',function(e){
     });
 });
 
+console.log(historyEl.getElementsByTagName('button'));
+// Create event for the button in search history
+
+Array.from(historyEl.getElementsByTagName('button')).forEach(function(el){
+    el.addEventListener("click", function(btnEl){
+        btnEl.preventDefault();
+        todayweatherEl.classList.remove("hide");
+        forecastweatherEl.classList.remove("hide");
+
+        console.log(btnEl); //The action "click"
+        console.log(btnEl.textContent); // Return undefined
+        console.log(el); // The button itself
+        console.log(el.textContent); // Return text value inside the button
+
+        var geoApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + el.textContent + "&limit=5&appid=" + key;
+        fetch(geoApiURL)
+        .then(function(response){
+            return response.json()
+        }).then(function(dataGeo){
+            var weatherApiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + dataGeo[0].lat + "&lon=" + dataGeo[0].lon +"&appid=" + key;
+        
+            fetch(weatherApiURL)
+            .then(function(response){
+                return response.json()
+            }).then(function(data){
+                todayweatherEl.getElementsByClassName("card-title")[0].innerHTML = dataGeo[0].local_names.en + ", " + dataGeo[0].country + " (" + dayjs().format("DD/MM/YYYY") + ")";
+                var todayweatherImg = document.createElement('img');
+                todayweatherImg.src = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+                todayweatherEl.getElementsByClassName("card-title")[0].appendChild(todayweatherImg);
+
+                todayweatherEl.getElementsByTagName("p")[0].innerHTML = "Temp: " + Math.round((data.list[0].main.temp - 273.15)*100)/100 + " \u00B0C"; // Convert to Celcius degree
+                todayweatherEl.getElementsByTagName("p")[1].innerHTML = "Wind: " + data.list[0].wind.speed + " KPH";
+                todayweatherEl.getElementsByTagName("p")[2].innerHTML = "Humidity: " + data.list[0].main.humidity + "%";
+
+                for (let i = 0; i < forecastweatherEl.getElementsByClassName("card-title").length;i++){
+                    forecastweatherEl.getElementsByClassName("card-title")[i].innerHTML = dayjs().add(i+1,'day').format("DD/MM/YYYY");
+                    forecastweatherEl.getElementsByClassName("weather-icon")[i].src = "http://openweathermap.org/img/w/" + data.list[i*8+7].weather[0].icon + ".png";
+                    forecastweatherEl.getElementsByClassName("temperature")[i].innerHTML = "Temp: " + Math.round((data.list[i*8+7].main.temp - 273.15)*100)/100 + " \u00B0C";
+                    forecastweatherEl.getElementsByClassName("wind")[i].innerHTML = "Wind: " + data.list[i*8 + 7].wind.speed + " KPH";
+                    forecastweatherEl.getElementsByClassName("humidity")[i].innerHTML = "Humidity: " + data.list[i*8+7].main.humidity + "%";
+                }
+            })
+        })
+    })
+})
