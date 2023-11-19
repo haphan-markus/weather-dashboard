839052d7ee8a7e3fd6b7a962cec3bfd93e8f661b
Ha Phan <ha.markus.phan@gmail.com>
2023-11-19 14:07:09 +0000

    test addEventListener()

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,28 @@
+var searchBtnEl = document.getElementById('search-button');
+var searchInputEl = document.getElementById("search-input");
+// console.log(searchBtnEl);
+// console.log(searchInputEl);
+// var searchInputText = searchInputEl.value.trim();
+searchBtnEl.addEventListener('click',function(e){
+    e.preventDefault();
+    var searchInputText = searchInputEl.value.trim();
+    console.log(searchInputEl);
+    console.log(searchInputText);
+    
+});
+
 
 var key = "87251923eddd66bd3c797f59bf9006ff";
 var latitude = 51.5072;
 var longitude = 0.1276;
 
-var apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude+"&appid=" + key;
+var geoApiURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+
+var weatherApiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude+"&appid=" + key;
 
 fetch(apiURL)
 .then(function(response){
     return response.json();
 }).then(function(data){
-    console.log(data);
+    // console.log(data);
 });
