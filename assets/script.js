e8c48d5c72663949c3c0f72084080c2cfd37e7af
Ha Phan <ha.markus.phan@gmail.com>
2023-11-20 20:27:19 +0000

    add clear search button

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ var key = "87251923eddd66bd3c797f59bf9006ff"; //API key
 var todayweatherEl = document.getElementById("today");
 var forecastweatherEl = document.getElementById("forecast");
 var historyEl = document.getElementById("history");
+var clearBtnEl = document.getElementById("clear-search-button");
 
 if (JSON.parse(window.localStorage.getItem(("searchHistory"))) == null){
     var searchHistory = [];
@@ -16,7 +17,6 @@ if (JSON.parse(window.localStorage.getItem(("searchHistory"))) == null){
         historyBtnEl.setAttribute('class','btn btn-secondary search-button form-control');
         historyEl.appendChild(historyBtnEl);
     }
-    console.log(historyEl);
 }
 
 searchBtnEl.addEventListener('click',function(e){
@@ -34,39 +34,30 @@ searchBtnEl.addEventListener('click',function(e){
         console.log(dataGeo);
         var weatherApiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + dataGeo[0].lat + "&lon=" + dataGeo[0].lon +"&appid=" + key;
         
-        // Create buttons for search results in div history
-        var searchHistory = JSON.parse(window.localStorage.getItem(("searchHistory"))) || [];
+        // Create buttons for search results in div "history"
         var historyBtnEl = document.createElement('button');
         historyBtnEl.innerHTML = dataGeo[0].local_names.en;
-        historyBtnEl.setAttribute('class','btn btn-secondary search-button form-control');
-        historyEl.appendChild(historyBtnEl);
         searchHistory.push(dataGeo[0].local_names.en);
+        historyBtnEl.setAttribute('class','btn btn-secondary search-button form-control');     
         window.localStorage.setItem("searchHistory",JSON.stringify(searchHistory));
+        historyEl.appendChild(historyBtnEl);
 
         fetch(weatherApiURL)
         .then(function(response){
             return response.json();
         }).then(function(data){
             presentTodayWeather(todayweatherEl,dataGeo,data); // call function to present the weather for today
-            
             present5dayForecast(forecastweatherEl,data); // call function to present the forecast for the next 5 day
         });
     });
 });
 
-console.log(historyEl.getElementsByTagName('button'));
 // Create event for the button in search history
-
 Array.from(historyEl.getElementsByTagName('button')).forEach(function(el){
     el.addEventListener("click", function(btnEl){
         btnEl.preventDefault();
         todayweatherEl.classList.remove("hide");
         forecastweatherEl.classList.remove("hide");
-
-        console.log(btnEl); //The action "click"
-        console.log(btnEl.textContent); // Return undefined
-        console.log(el); // The button itself
-        console.log(el.textContent); // Return text value inside the button
 
         var geoApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + el.textContent + "&limit=5&appid=" + key;
         fetch(geoApiURL)
@@ -74,17 +65,20 @@ Array.from(historyEl.getElementsByTagName('button')).forEach(function(el){
             return response.json()
         }).then(function(dataGeo){
             var weatherApiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + dataGeo[0].lat + "&lon=" + dataGeo[0].lon +"&appid=" + key;
-        
+    
             fetch(weatherApiURL)
             .then(function(response){
                 return response.json()
             }).then(function(data){
                 presentTodayWeather(todayweatherEl,dataGeo,data); // call function to present the weather for today
-                
                 present5dayForecast(forecastweatherEl,data); // call function to present the forecast for the next 5 day
             })
         })
     })
+})
+
+clearBtnEl.addEventListener("click",function(){
+    window.localStorage.clear();
 })
 
 function presentTodayWeather(todayEl,geoApiData,apiData){
